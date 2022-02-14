7bb6dd2b61582887395b297041a921ed6ea2365a
Othniel <83201932+Carrot1048@users.noreply.github.com>
2022-02-13 22:46:22 -0600

    Add files via upload

---
+++ JavaAssignment/Wolf.js
@@ -0,0 +1,23 @@
+let wolf;
+let moon;
+let y = 540;
+
+function setup() {
+  let cnv = createCanvas(960, 590);
+  cnv.id('Cover');
+  background(29, 44, 59);
+  imageMode(CENTER);
+  wolf = loadImage("wolf.png");
+  moon = loadImage("moon.jpg");
+}
+
+function draw() {
+  image(wolf, width/2, 540, 108, 100);
+  push();
+  image(moon, width/2, y, 100, 100);
+  y+=-10;
+  if(y<50){
+    y+=10;
+  }
+  pop();
+}
