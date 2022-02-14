4219c696c73b89467729b1b741bd70b0d68d04eb
Othniel <83201932+Carrot1048@users.noreply.github.com>
2022-02-13 22:54:56 -0600

    Add files via upload

--- JavaAssignment/Wolf.js
+++ JavaAssignment/Wolf.js
@@ -15,10 +15,14 @@ function draw() {
   image(wolf, width/2, 540, 108, 100);
   push();
   translate(0, y);
-  image(moon, width/2, 540, 100, 100);
   y+=-10;
   if(y<50){
     y+=10;
   }
+  fullmoon();
   pop();
+}
+
+function fullmoon() {
+  image(moon, width/2, 540, 100, 100);
 }
